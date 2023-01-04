24df0f8d347657ee8ddd91ecde97d56ca40acc08
abhishek96negi <abhisheknegi@rapidinnovation.dev>
2023-01-04 16:54:21 +0530

    PYT-45: update todolist.sol
    1. Rename the variable as _tasks
    2. instead of writing uint, write uint256 which tells about the exact datatype.
    3. rename taskId to taskId_
    4. Missing the License Identifier
    5.Fix the contract version >= 0.8 because it has SafeMath support out of the box.

--- src/contract/todolist.sol
+++ src/contract/todolist.sol
@@ -1,32 +1,39 @@
-pragma solidity ^0.4.17;   // Specifies the version of solidity that our code is written with
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;  // Specifies the version of solidity that our code is written with
 
 contract TodoList {
     // A mapping of task IDs to task descriptions
-    mapping(uint => string) private tasks;
+    mapping(uint => string) private _tasks;
 
     // The next task ID to assign
-    uint public nextTaskId;
+    uint256 public nextTaskId;
 
     // Adds a new task to the list
     function addTask(string memory description) public {
-        tasks[nextTaskId] = description;
+        _tasks[nextTaskId] = description;
         nextTaskId++;
     }
 
     // Get a task from the list.
-    function getTask(uint taskId) public view returns(string memory){
-        require(bytes(tasks[taskId]).length != 0, "Invalid Task ID");
-        return tasks[taskId];
+    function getTask(uint256 taskId_) public view returns (string memory){
+        require(bytes(_tasks[taskId_]).length != 0, "Invalid Task ID");
+        return _tasks[taskId_];
     }
 
     // Removes a task from the list
-    function removeTask(uint taskId) public {
-        delete tasks[taskId];
+    function removeTask(uint256 taskId_) public {
+        delete _tasks[taskId_];
     }
 
     // Update a task from the list.
-    function updateTask(uint taskId, string text_) public {
-        tasks[taskId] = text_;
+    function updateTask(uint256 taskId_, string memory text_) public {
+        _tasks[taskId_] = text_;
     }
-}
 
+    // Total task count from the list.
+    function totalCountTask() public pure returns (uint){
+        //   return _tasks;
+    }
+
+}
