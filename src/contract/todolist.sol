318e839cfd7a22a26d6cc040afd5828ff2fc8224
abhishek96negi <abhisheknegi@rapidinnovation.dev>
2023-01-05 11:41:30 +0530

    PYT-45: update todolist.sol

--- src/contract/todolist.sol
+++ src/contract/todolist.sol
@@ -2,38 +2,75 @@
 
 pragma solidity 0.8.0;  // Specifies the version of solidity that our code is written with
 
-contract TodoList {
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TodoList is ReentrancyGuard, Ownable{
+
+    // todo list status
+    enum TodoStatuses {
+        PENDING,
+        COMPLETE
+    }
+
+    // todo list record
+    struct Todo{
+
+        // tasks id
+        uint256 id;
+
+        // task title
+        string title;
+
+        // task status
+        TodoStatuses status;
+
+    }
+
+    TodoStatuses private constant _DEFAULT_CHOICE = TodoStatuses.PENDING;
+
+    Todo[] private _listOfTodos;
+
     // A mapping of task IDs to task descriptions
-    mapping(uint => string) private _tasks;
+    // mapping(uint => string) private _tasks;
 
     // The next task ID to assign
-    uint256 public nextTaskId;
+    uint256 private nextTaskId = 1;
 
     // Adds a new task to the list
-    function addTask(string memory description) public {
-        _tasks[nextTaskId] = description;
+    function addTask(string memory title_) external onlyOwner nonReentrant{
+        Todo memory todo = Todo(nextTaskId, title_,  _DEFAULT_CHOICE);
+        _listOfTodos.push(todo);
         nextTaskId++;
     }
 
     // Get a task from the list.
-    function getTask(uint256 taskId_) public view returns (string memory){
-        require(bytes(_tasks[taskId_]).length != 0, "Invalid Task ID");
-        return _tasks[taskId_];
+    function getTask(uint256 taskId_) public view returns (Todo memory){
+        return _listOfTodos[taskId_];
+    }
+
+    function markComplete(uint taskId_) external onlyOwner nonReentrant{
+        /**
+         * Mark a task as complete
+         */
+        _listOfTodos[taskId_].status = TodoStatuses.COMPLETE;
     }
 
     // Removes a task from the list
-    function removeTask(uint256 taskId_) public {
-        delete _tasks[taskId_];
+    function removeTask(uint256 taskId_) external onlyOwner nonReentrant{
+        require(taskId_ <= _listOfTodos.length, "Tasks are less");
+        _listOfTodos[taskId_] = _listOfTodos[_listOfTodos.length - 1];
+        _listOfTodos.pop();
     }
 
     // Update a task from the list.
-    function updateTask(uint256 taskId_, string memory text_) public {
-        _tasks[taskId_] = text_;
+    function updateTask(uint256 taskId_, string memory text_) external onlyOwner nonReentrant{
+        _listOfTodos[taskId_].title = text_;
     }
 
     // Total task count from the list.
-    function totalCountTask() public pure returns (uint){
-        //   return _tasks;
+    function totalCountTask() public view returns (uint){
+        return _listOfTodos.length;
     }
 
 }
